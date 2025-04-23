f9920877f2479ea1229aa24287db422699042492
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-04-22 21:28:20 -0400

    Relized I didn't have auto save on... New Math fuctions, Custom math functions

---
+++ FunctionsUntil/CustomMath.java
@@ -0,0 +1,68 @@
+package FunctionsUntil;
+
+public class CustomMath {
+    /**
+     * Finds the value of a number to the power of another number
+     * @param num Inital Number
+     * @param powerOf The power of the number
+     * @return Value of the inital number to the power of inputed number
+     */
+    public static double pow(double num, double powerOf){
+        double poweredNum = num;
+
+        for (int i = 0; i <= abVal(powerOf); i++){
+            poweredNum *= num;
+        }
+        if(powerOf < 0) return 1/poweredNum;
+        return poweredNum;
+    }
+
+    /**
+     * Finds the absolute value of the given number:
+     * @param initNum Intial Number
+     * @return Absolute number of inputed value
+     */
+    public static double abVal(double initNum){
+        //If the number is negative inverse the number
+        if (initNum<0) return initNum*-1;
+        //returns same value otherwise
+        else return initNum;
+    }
+
+    //  Used AI for assitance in the creation of the custom square root function
+    /**
+     * 
+     * @param squareNum the squared inputed number
+     * @param precision accuraccy of number
+     * @return square root of inputed number
+     */
+    public static double customSqrt(double squareNum, double precision) {
+        // Checks if number is negative
+        if (squareNum < 0) {
+            throw new IllegalArgumentException("Cannot compute square root of negative number");
+        }
+        if (squareNum == 0 || squareNum == 1) {
+            return squareNum;
+        }
+        double rootedNum = squareNum;
+        while (abVal(rootedNum * rootedNum - squareNum) > precision) {
+            rootedNum = (rootedNum + squareNum / rootedNum) / 2.0;
+        }
+        return rootedNum;
+    }
+    public static double customSqrt(double squareNum){
+        double precision = 1e-10;
+        // Checks if number is negative
+        if (squareNum < 0) {
+            throw new IllegalArgumentException("Cannot compute square root of negative number");
+        }
+        if (squareNum == 0 || squareNum == 1) {
+            return squareNum;
+        }
+        double rootedNum = squareNum;
+        while (abVal(rootedNum * rootedNum - squareNum) > precision) {
+            rootedNum = (rootedNum + squareNum / rootedNum) / 2.0;
+        }
+        return rootedNum;
+    }
+}
--- FunctionsUntil/geometry.java
+++ FunctionsUntil/geometry.java
@@ -1,7 +1,51 @@
 package FunctionsUntil;
+import FunctionsUntil.CustomMath;
 
 public class geometry {
+
+public class triangle{
+
     public static double triangleArea(double length, double width){
         return (length * width)/2;
     }
+    /**
+     * Uses Pythagorean Theorem to find the values of each side of a triangle
+     * @param values Value of each side mapped to an index in assending order
+     * @param missingValue The side the user want to find
+     * @return The list containing the values of each sides value
+     */
+    public static double[] pythagoreanTheorem(double[] values, String missingValue){
+        double a, b, c;
+        a = values[0];
+        b = values[1];
+        c = values[2];
+        switch(missingValue){
+            case "a":
+                //c squared - b squared
+                a = CustomMath.pow(c, 2)- CustomMath.pow(b, 2);
+                //square root of a
+                a = CustomMath.customSqrt(a);
+            break;
+            case "b":
+                //c squared - a squared
+                b = CustomMath.pow(c, 2)- CustomMath.pow(a, 2);
+                //square root of b
+                b = CustomMath.customSqrt(b);
+            break;
+            case "c":
+                //a squared + b squared
+                c = CustomMath.pow(a, 2) + CustomMath.pow(b, 2);
+                //square root of c
+                c = CustomMath.customSqrt(c);
+            break;
+        }
+
+        values[0] = a;
+        values[1] = b;
+        values[2] = c;
+
+        return values;
+    }
+}
+
 }
